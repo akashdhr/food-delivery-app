9029c270a819d13ee3f874e747c61246b3ccbcfb
Akash Dhar <akshdhr117@gmail.com>
2023-03-14 01:00:49 +0530

    Adding Header Components

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
+import Header from "./Layout/Header";
 
 function App() {
   return (
-    <div className="App">
-      <h2>Let's get Started</h2>
-    </div>
+    <>
+      <Header />
+    </>
   );
 }
 
---
+++ src/Layout/Header.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import mealsImage from '../assets/meals.jpeg';
+import classes from './Header.module.css';
+import HeaderCartButton from './HeaderCartButton';
+
+const Header = () => {
+  return (
+    <>
+        <header className={classes.header}>
+            <h1>Meals</h1>
+            <HeaderCartButton />
+        </header>
+        <div className={classes["main-image"]}>
+            <img src={mealsImage} alt="Table full of delicious food"/>
+        </div>
+    </>
+  )
+}
+
+export default Header
