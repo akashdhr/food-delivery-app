26974df4016fce00db065c2d21f87f05c2ee7ebe
Akash Dhar <akshdhr117@gmail.com>
2023-03-17 01:22:33 +0530

    Adding Cart Open/Close Logic

--- src/App.js
+++ src/App.js
@@ -1,10 +1,24 @@
 import Header from "./Layout/Header";
 import Meals from "./Meals/Meals";
+import Cart from "./Cart/Cart";
+import { useState } from "react";
 
 function App() {
+
+  const [cartIsShown, setCartIsShown] = useState(false)
+
+  const showCartHandler =() => {
+    setCartIsShown(true);
+  }
+
+  const closeCartHandler = () =>{
+    setCartIsShown(false);
+  }
+
   return (
     <>
-      <Header />
+      {cartIsShown && <Cart onCloseCart={closeCartHandler}/> }
+      <Header onShowCart={showCartHandler}/>
       <main>
         <Meals />
       </main>
